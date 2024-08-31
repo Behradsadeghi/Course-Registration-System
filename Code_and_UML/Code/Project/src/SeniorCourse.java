50534cba865eab74074f869cd285967ac5575bb6
Behrad <behrad@Behrads-MacBook-Air.local>
2024-08-31 07:01:54 +0330

    add code and uml

---
+++ Code_and_UML/Code/Project/src/SeniorCourse.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class SeniorCourse extends Course implements Serializable {
+
+    protected static final long SERIAL_VERSION_UID = 14L;
+
+    public SeniorCourse(int id, String name) {
+        super(id, name);
+    }
+    public SeniorCourse(int id, String name, int weight, int courseTime, int capacity, int inputYear, Professor professor) {
+        super(id, name, weight, courseTime, capacity, inputYear, professor);
+    }
+    public int getMax() {
+        return maxCapacity;
+    }
+    public void setMax(int max) {
+        maxCapacity = max;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
